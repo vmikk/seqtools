fbe3084ef4d460e95fd0710e9a24747946baa386
Vladimir Mikryukov <v.mikryukov@gmail.com>
2025-07-13 16:12:45 +0300

    vite config

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          state: ['zustand'],
+          ui: ['lucide-react', 'sonner']
+        }
+      }
+    }
+  },
+})
